a49e08dc133c7ec6217cac09c16191bfd3baaeb2
Rahul Khatri <53160725+rahulk93@users.noreply.github.com>
2026-04-11 23:34:12 +1200

    Designing the Timeline/Home Page!

--- SocialMediaCircleApp/Controllers/HomeController.cs
+++ SocialMediaCircleApp/Controllers/HomeController.cs
@@ -10,16 +10,5 @@ namespace SocialMediaCircleApp.Controllers
         {
             return View();
         }
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
     }
 }
