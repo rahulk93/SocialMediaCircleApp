9e09c9214903835d511cafbee35a2cb048e61cc1
Rahul Khatri <53160725+rahulk93@users.noreply.github.com>
2026-04-23 10:55:15 +1200

    Removing Posts!

--- SocialMediaCircleApp.Data/Models/Post.cs
+++ SocialMediaCircleApp.Data/Models/Post.cs
@@ -13,6 +13,7 @@ namespace SocialMediaCircleApp.Data.Models
         public bool IsPrivate { get; set; }
         public DateTime DateCreated { get; set; }
         public DateTime DateUpdated { get; set; }
+        public bool IsDeleted { get; set; }
 
 
 
