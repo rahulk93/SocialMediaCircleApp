f9567482319c2d7cc59fe6144780dff58d68e6de
Rahul Khatri <53160725+rahulk93@users.noreply.github.com>
2026-06-04 11:01:17 +1200

    Building & Managing Friendships: Requests, Suggestions, and Connections!

---
+++ SocialMediaCircleApp/ViewModels/Friends/FriendshipVM.cs
@@ -0,0 +1,11 @@
+﻿using SocialMediaCircleApp.Data.Models;
+
+namespace SocialMediaCircleApp.ViewModels.Friends
+{
+    public class FriendshipVM
+    {
+        public List<Friendship> Friends = new List<Friendship>();
+        public List<FriendRequest> FriendRequestsSent = new List<FriendRequest>();
+        public List<FriendRequest> FriendRequestsReceived = new List<FriendRequest>();
+    }
+}
